79e7dc5f51c34fc3b7554b63399d4c5cf7cdd37f
samsonsilas <samsonsilas781@gmail.com>
2023-01-08 10:17:09 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,117 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+
+interface ansType {
+    username: string,
+    userpin:number,
+    accountType:string,
+    transactionType:string,
+    amount:number
+}
+
+const toDoList : string[]= [];
+
+const sleep = ()=>{
+    return new Promise((res)=>{
+        setTimeout(res,2000)
+    })
+}
+
+async function welcome() {
+    let title = await chalkAnimation.rainbow(`|||     ATM MACHINE    |||`);
+     await sleep();
+     title.stop()
+   
+}
+
+await welcome()
+
+async function questions(){
+  
+
+const answers : ansType = await inquirer.prompt([
+    {
+        type:'input',
+        name:'username',
+        message:'username',
+        
+    },
+     {
+        type:'number',
+        name:'userpin',
+        message:'pin',
+        
+    },
+     {
+        type:'list',
+        name:'accountType',
+        choices:["Current","Saving"],
+        message:'select your account type',
+        
+    },
+       {
+        type:'list',
+        name:'transactionType',
+        choices:["Fast Cash","Withdraw cash","Balance inquirey"],
+        message:'select your transaction  type',
+        when(answers){
+            return answers.accountType
+        }
+        
+    },
+      {
+        type:'list',
+        name:'amount',
+        choices:[1000,2000,10000,20000],
+        message:'select your amount',
+        when(answers){
+            return answers.transactionType==="Fast Cash"
+        }
+        
+    },
+
+      {
+        type:'number',
+        name:'amount',
+        message:'enter your amount',
+        when(answers){
+            return answers.transactionType==="Withdraw cash"
+        }
+        
+    }
+])
+
+const {username,userpin,transactionType,accountType,amount} = answers;
+
+
+if(username && userpin){
+
+    const randomBalance =Math.floor(Math.random()*100000000);
+   if(transactionType==="Balance inquirey") {
+    console.log( chalk.grey('Your Balance is ', randomBalance));
+
+
+   }
+   else{
+    if(randomBalance > amount){
+
+        const remainingBalance = randomBalance-amount;
+        console.log('Your ramaining Balance is ',randomBalance);
+    }
+
+    else{
+
+        console.log( chalk.red('insuffient Balance'))
+    }   
+   }
+
+}
+
+}
+
+
+questions();
+
